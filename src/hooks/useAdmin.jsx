4e697eaf2c310b04e44f62cad1e956ba4ebe7e12
rajibrahman74e <rajiburrahman74@gmail.com>
2023-06-11 05:12:40 +0600

    [ADD]-make-normal-user-to-admin

---
+++ src/hooks/useAdmin.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProviders";
+import useAxiosSecure from "./useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+
+const useAdmin = () => {
+  const { user, loading } = useContext(AuthContext);
+  const [axiosSecure] = useAxiosSecure();
+  
+  // use axios secure with react query
+  const { data: isAdmin, isLoading: isAdminLoading } = useQuery({
+    queryKey: ["isAdmin", user?.email],
+    enabled: !loading,
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/admin/${user?.email}`);
+      return res.data.admin;
+    },
+  });
+  return [isAdmin, isAdminLoading];
+};
+export default useAdmin;
--- src/pages/Dashboard/AdminHome/ManageUsers/ManageUsers.jsx
+++ src/pages/Dashboard/AdminHome/ManageUsers/ManageUsers.jsx
@@ -3,6 +3,7 @@ import { FaAd, FaUserShield } from "react-icons/fa";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet";
+import Swal from "sweetalert2";
 
 const ManageUsers = () => {
   // const users = useLoaderData();
@@ -13,6 +14,26 @@ const ManageUsers = () => {
     console.log(res.data);
     return res.data;
   });
+
+  const handleMakeAdmin = (user) => {
+    fetch(`http://localhost:5000/users/admin/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-start",
+            icon: "success",
+            title: `${user.name} is and admin now`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
 
   return (
     <div className="mb-12">
@@ -60,8 +81,14 @@ const ManageUsers = () => {
                   </button>
                 </td>
                 <td>
-                  <button className="btn text-lg btn-ghost ">
+                  {/* <button className="btn text-lg btn-ghost ">
                     <FaUserShield></FaUserShield>
+                  </button> */}
+                  <button
+                    onClick={() => handleMakeAdmin(user)}
+                    className="btn btn-ghost btn-xl bg:hover-none bg-none border-0"
+                  >
+                    {user.role === "admin" ? "Admin" : <FaUserShield />}
                   </button>
                 </td>
               </tr>
