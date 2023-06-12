a318d61a65b845efd1274a7c58df41960077aa39
rajibrahman74e <rajiburrahman74@gmail.com>
2023-06-12 14:01:04 +0600

    [ADD&FIX]-add-redmi-file-and-fix-classes-conditinal-issue

---
+++ README.md
@@ -0,0 +1,62 @@
+# SportsHub academy
+
+## Description
+
+This project is a React-based web application that combines various technologies and libraries to provide user authentication with Firebase Auth, responsive UI design with Tailwind CSS, data storage with MongoDB and Node.js, and interactive page transitions using the Swiper library.
+
+## Motivation
+
+The motivation behind this project was to create a robust and engaging web application by leveraging modern technologies and tools. The goal was to enhance the user experience, simplify the development process, and provide a solid foundation for building scalable and feature-rich applications.
+
+## Features
+
+- User authentication and authorization with Firebase Auth.
+- Responsive UI design utilizing Tailwind CSS for efficient styling and layout.
+- Data storage and retrieval using MongoDB with Node.js.
+- Interactive and smooth page transitions with the Swiper library.
+- Three pages included as a starting point for further development.
+- A normal user can select a class and delete the class from the selected class.
+- An instructor can add a new class. If the admin accepts it.
+- And an administrator can approve, deny, and provide feedback to classes if desired.
+
+## Installation
+
+1. Install the dependencies: `npm install`
+2. Configure Firebase Auth credentials: Add your Firebase configuration in the `.env` file.
+3. Configure MongoDB connection: Update the MongoDB connection string in the server configuration files.
+4. Start the development server: `npm run dev`
+
+Note: Ensure you have Node.js and MongoDB installed on your system before proceeding with the installation.
+
+## Usage
+
+1. After starting the development server, the application will be accessible at `http://localhost:5173/`.
+2. Sign up or log in to access the protected pages and utilize the application's features.
+3. Explore the different pages and functionalities to get a better understanding of the project structure and implementation.
+4. Modify and extend the project as needed for your specific requirements.
+
+## Contributing
+
+Contributions are welcome! If you find any bugs, have suggestions for improvements, or want to add new features, feel free to open issues or submit pull requests. Please adhere to the project's code style and guidelines.
+
+## Acknowledgments
+
+- [React](https://reactjs.org/)
+- [Firebase](https://firebase.google.com/)
+- [Tailwind CSS](https://tailwindcss.com/)
+- [daisyui](https://daisyui.com/)
+- [Node.js](https://nodejs.org/)
+- [MongoDB](https://www.mongodb.com/)
+- [Swiper](https://swiperjs.com/)
+
+Special thanks to the creators and contributors of these amazing technologies and libraries that made this project possible.
+
+## Webiste Links
+
+- [Live site]() -[Servr side]()
+
+## Contact
+
+For any inquiries or feedback, please reach out to [rajiburrahman74@gmail.com](mailto:rajiburrahman74@gmail.comm).
+
+Happy coding!
--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -1,133 +1,133 @@
-// import React, { useContext } from "react";
-// import { Helmet } from "react-helmet";
-// import useData from "../../hooks/useData";
-// import useAxiosSecure from "../../hooks/useAxiosSecure";
-// import { AuthContext } from "../../providers/AuthProviders";
-// import Swal from "sweetalert2";
-// import useAdmin from "../../hooks/useAdmin";
-// import useInstructor from "../../hooks/useInstructor";
+import React, { useContext } from "react";
+import { Helmet } from "react-helmet";
+import useData from "../../hooks/useData";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { AuthContext } from "../../providers/AuthProviders";
+import Swal from "sweetalert2";
+import useAdmin from "../../hooks/useAdmin";
+import useInstructor from "../../hooks/useInstructor";
 
-// const Classes = () => {
-//   const [data] = useData();
-//   const [axiosSecure] = useAxiosSecure();
-//   const { user } = useContext(AuthContext);
-//   const [isAdmin] = useAdmin();
-//   const [isInstructor] = useInstructor();
+const Classes = () => {
+  const [data] = useData();
+  const [axiosSecure] = useAxiosSecure();
+  const { user } = useContext(AuthContext);
+  const [isAdmin] = useAdmin();
+  const [isInstructor] = useInstructor();
 
-//   const handleSelectClass = (cls) => {
-//     const { class_name, class_image, instructor_name, price } = cls;
-//     if (user?.email) {
-//       const addedClass = {
-//         userEmail: user?.email,
-//         class_name,
-//         class_image,
-//         instructor_name,
-//         price,
-//       };
-//       axiosSecure
-//         .post("/selectedclass", addedClass)
-//         .then((data) => {
-//           console.log(data.data.insertedId);
-//           if (data.data.insertedId) {
-//             Swal.fire({
-//               position: "center",
-//               icon: "success",
-//               title: "Selected successfully",
-//               showConfirmButton: false,
-//               timer: 1500,
-//             });
-//           }
-//         })
-//         .catch((err) => console.log(err));
-//     }
-//   };
+  const handleSelectClass = (cls) => {
+    const { class_name, class_image, instructor_name, price } = cls;
+    if (user?.email) {
+      const addedClass = {
+        userEmail: user?.email,
+        class_name,
+        class_image,
+        instructor_name,
+        price,
+      };
+      axiosSecure
+        .post("/selectedclass", addedClass)
+        .then((data) => {
+          console.log(data.data.insertedId);
+          if (data.data.insertedId) {
+            Swal.fire({
+              position: "center",
+              icon: "success",
+              title: "Selected successfully",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  };
 
-//   const handleButtonClick = (item) => {
-//     if (user) {
-//       handleSelectClass(item);
-//     } else {
-//       Swal.fire({
-//         position: "top-start",
-//         icon: "error",
-//         title: "Please log in first",
-//         showConfirmButton: false,
-//         timer: 500,
-//       }).then(() => {
-//         window.location.href = "/login";
-//       });
-//     }
-//   };
+  const handleButtonClick = (item) => {
+    if (user) {
+      handleSelectClass(item);
+    } else {
+      Swal.fire({
+        position: "top-start",
+        icon: "error",
+        title: "Please log in first",
+        showConfirmButton: false,
+        timer: 500,
+      }).then(() => {
+        window.location.href = "/login";
+      });
+    }
+  };
 
-//   return (
-//     <div className="max-w-7xl mx-auto px-4 my-20">
-//       <Helmet>
-//         <title>Classes - SportsHub Academy</title>
-//       </Helmet>
-//       <h2 className="font-bold text-center text-3xl mb-16">All Classes</h2>
-//       <div className="grid grid-cols-1 md:grid-cols-3 gap-16">
-//         {data.map((item) => (
-//           <div
-//             key={item._id}
-//             className={`overflow-hidden rounded bg-white text-slate-500 shadow-md shadow-slate-200 ${
-//               item.available_seats <= 0
-//                 ? "border-[3px] border-red-600"
-//                 : "border-0"
-//             }`}
-//           >
-//             {/* Image */}
-//             <figure>
-//               <img
-//                 src={item.class_image}
-//                 alt="card image"
-//                 className="aspect-video w-full"
-//               />
-//             </figure>
-//             {/* Body */}
-//             <div className="p-6">
-//               <header className="mb-4">
-//                 <h3 className="text-xl font-medium text-slate-700">
-//                   {item.class_name}
-//                 </h3>
-//                 <p className="font-semibold text-black text-md my-3">
-//                   {item.instructor_name}
-//                 </p>
-//                 <div className="flex justify-between items-center">
-//                   <span className="text-slate-400">Price: ${item.price}</span>
-//                   <span className="text-slate-400">
-//                     Available seats: {item.available_seats}
-//                   </span>
-//                 </div>
-//               </header>
-//             </div>
-//             {/* Action base sized basic button */}
-//             <div className="flex justify-end p-6 pt-0">
-//               {user && (isAdmin || isInstructor) ? (
-//                 <button
-//                   onClick={() => handleButtonClick(item)}
-//                   className="inline-flex h-10 w-full items-center justify-center gap-2 whitespace-nowrap rounded-none bg-warning px-5 text-sm font-medium tracking-wide text-white opacity-50 cursor-not-allowed"
-//                   disabled
-//                 >
-//                   <span>Select class</span>
-//                 </button>
-//               ) : (
-//                 <button
-//                   onClick={() => handleButtonClick(item)}
-//                   className={`inline-flex h-10 w-full items-center justify-center gap-2 whitespace-nowrap rounded-none bg-warning px-5 text-sm font-medium tracking-wide text-white ${
-//                     item.available_seats <= 0
-//                       ? "opacity-50 cursor-not-allowed"
-//                       : ""
-//                   }`}
-//                   disabled={item.available_seats <= 0}
-//                 >
-//                   <span>Select class</span>
-//                 </button>
-//               )}
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className="max-w-7xl mx-auto px-4 my-20">
+      <Helmet>
+        <title>Classes - SportsHub Academy</title>
+      </Helmet>
+      <h2 className="font-bold text-center text-3xl mb-16">All Classes</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-16">
+        {data.map((item) => (
+          <div
+            key={item._id}
+            className={`overflow-hidden rounded bg-white text-slate-500 shadow-md shadow-slate-200 ${
+              item.available_seats <= 0
+                ? "border-[3.5px] border-red-600"
+                : "border-0"
+            }`}
+          >
+            {/* Image */}
+            <figure>
+              <img
+                src={item.class_image}
+                alt="card image"
+                className="aspect-video w-full"
+              />
+            </figure>
+            {/* Body */}
+            <div className="p-6">
+              <header className="mb-4">
+                <h3 className="text-xl font-medium text-slate-700">
+                  {item.class_name}
+                </h3>
+                <p className="font-semibold text-black text-md my-3">
+                  {item.instructor_name}
+                </p>
+                <div className="flex justify-between items-center">
+                  <span className="text-slate-400">Price: ${item.price}</span>
+                  <span className="text-slate-400">
+                    Available seats: {item.available_seats}
+                  </span>
+                </div>
+              </header>
+            </div>
+            {/* Action base sized basic button */}
+            <div className="flex justify-end p-6 pt-0">
+              {user && (isAdmin || isInstructor) ? (
+                <button
+                  onClick={() => handleButtonClick(item)}
+                  className="inline-flex h-10 w-full items-center justify-center gap-2 whitespace-nowrap rounded-none bg-warning px-5 text-sm font-medium tracking-wide text-white opacity-50 cursor-not-allowed"
+                  disabled
+                >
+                  <span>Select class</span>
+                </button>
+              ) : (
+                <button
+                  onClick={() => handleButtonClick(item)}
+                  className={`inline-flex h-10 w-full items-center justify-center gap-2 whitespace-nowrap rounded-none bg-warning px-5 text-sm font-medium tracking-wide text-white ${
+                    item.available_seats <= 0
+                      ? "opacity-50 cursor-not-allowed"
+                      : ""
+                  }`}
+                  disabled={item.available_seats <= 0}
+                >
+                  <span>Select class</span>
+                </button>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-// export default Classes;
+export default Classes;
